72092770328659988998d5693c1a23a0dd74990f
Michel Albert <michel@albert.lu>
2007-10-14 10:16:37 +0000

    Initial SVN import
    git-svn-id: https://python-cluster.svn.sourceforge.net/svnroot/python-cluster/trunk@1 57eab859-f816-0410-af72-e61ffa1cc713

---
+++ clusterTests.py
@@ -0,0 +1,148 @@
+#
+# This is part of "python-cluster". A library to group similar items together.
+# Copyright (C) 2006   Michel Albert
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+from cluster import *
+from difflib import SequenceMatcher
+import unittest
+
+class HClusterSmallListTestCase(unittest.TestCase):
+   " Test for Bug #1516204 "
+
+   def testClusterLen1(self):
+      "Testing if hierarchical clustering a set of length 1 returns a set of length 1"
+      cl = HierarchicalClustering([876], lambda x,y: abs(x-y))
+      self.assertEqual([876], cl.getlevel(40))
+
+   def testClusterLen0(self):
+      "Testing if hierarchical clustering an empty list returns an empty list"
+      cl = HierarchicalClustering([], lambda x,y: abs(x-y))
+      self.assertEqual([], cl.getlevel(40))
+
+class HClusterIntegerTestCase(unittest.TestCase):
+
+   def setUp(self):
+      self.__data = [791, 956, 676, 124, 564, 84, 24, 365, 594, 940, 398,
+                     971, 131, 365, 542, 336, 518, 835, 134, 391]
+
+   def testCluster(self):
+      "Basic Hierarchical Clustering test with integers"
+      cl = HierarchicalClustering(self.__data, lambda x,y: abs(x-y))
+      cl.cluster()
+      self.assertEqual( [
+            [24],
+            [84, 124, 131, 134],
+            [336, 365, 365, 365, 398, 391],
+            [940, 956, 971],
+            [791],
+            [835],
+            [676],
+            [518, 564, 542]
+            ],
+            cl.getlevel(40))
+
+class HClusterStringTestCase(unittest.TestCase):
+
+   def sim(self, x, y):
+      sm = SequenceMatcher(lambda x: x in ". -", x, y)
+      return 1-sm.ratio()
+
+   def setUp(self):
+      self.__data = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut elit. Phasellus consequat ultricies mi. Sed congue leo at neque. Nullam.". split()
+
+   def testDataTypes(self):
+      "Test for bug #?"
+      cl = HierarchicalClustering(self.__data, self.sim)
+      for item in cl.getlevel(0.5):
+         self.assertEqual(
+               type(item), type([]),
+               "Every item should be a list!")
+
+   def testCluster(self):
+      "Basic Hierachical clustering test with strings"
+      cl = HierarchicalClustering(self.__data, self.sim)
+      self.assertEqual([
+               ['Nullam.'],
+               ['Sed'],
+               ['mi.'],
+               ['ultricies'],
+               ['Phasellus'],
+               ['amet,', 'at'],
+               ['sit', 'elit.', 'elit.', 'elit.'],
+               ['leo', 'Lorem', 'dolor'],
+               ['neque.', 'congue', 'consectetuer', 'consequat'],
+               ['ipsum'],
+               ['adipiscing']
+               ],
+            cl.getlevel(0.5)
+            )
+
+class KClusterSmallListTestCase(unittest.TestCase):
+
+   def testClusterLen1(self):
+      "Testing that a search space of length 1 returns only one cluster"
+      cl = KMeansClustering([876])
+      self.assertEqual([876], cl.getclusters(2))
+      self.assertEqual([876], cl.getclusters(5))
+
+   def testClusterLen0(self):
+      "Testing if clustering an empty set, returns an empty set"
+      cl = KMeansClustering([])
+      self.assertEqual([], cl.getclusters(2))
+      self.assertEqual([], cl.getclusters(7))
+
+class KCluster2DTestCase(unittest.TestCase):
+
+   def testClusterCount(self):
+      "Test that asking for less than 2 clusters raises an error"
+      cl = KMeansClustering([876, 123, 344, 676], distance=lambda x,y: abs(x-y))
+      self.assertRaises(ValueError, cl.getclusters, 0)
+      self.assertRaises(ValueError, cl.getclusters, 1)
+
+   def testUniformLength(self):
+      "Test if there is an item in the cluster that has a different cardinality"
+      data = [ (1,5), (2,5), (2,6), (3,4), (3,5), (3,6,7), (7,3), (8,1), (8,2), (8), (9,2), (9,3) ]
+      self.assertRaises(ValueError, KMeansClustering, data)
+
+   def testPointDoubling(self):
+      "test for bug ?"
+      data = [ (0,0), (7,3), (2,6), (3,5), (3,6), (0,0), (8,1), (3,4), (8,3), (9,2), (2,5), (9,3) ]
+      cl = KMeansClustering(data)
+      clusters = cl.getclusters(2)
+      self.assertEqual(
+            cl.getclusters(2),
+            [[(0, 0), (2, 6), (3, 6), (2, 5), (3, 5), (3, 4), (1, 5)],
+             [(7, 3), (9, 2), (9, 3), (8, 1), (8, 3)]])
+
+   def testClustering(self):
+      "Basic clustering test"
+      data = [ (8,2), (7,3), (2,6), (3,5), (3,6), (1,5), (8,1), (3,4), (8,3), (9,2), (2,5), (9,3) ]
+      cl = KMeansClustering(data)
+      clusters = cl.getclusters(2)
+      self.assertEqual(
+            cl.getclusters(2),
+            [[(8, 2), (8, 1), (8, 3), (7, 3), (9, 2), (9, 3)],
+             [(3, 5), (1, 5), (3, 4), (2, 6), (2, 5), (3, 6)]])
+
+unittest.TextTestRunner(verbosity=2).run(
+      unittest.TestSuite((
+            unittest.makeSuite(HClusterSmallListTestCase),
+            unittest.makeSuite(HClusterIntegerTestCase),
+            unittest.makeSuite(HClusterStringTestCase),
+            unittest.makeSuite(KClusterSmallListTestCase),
+            unittest.makeSuite(KCluster2DTestCase),
+         ))
+      )
