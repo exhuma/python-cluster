c1e5d9cd9222e3cf133e37e14d56995de332752e
Michel Albert <michel@albert.lu>
2013-03-08 19:25:03 +0100

    Removed wildcard import.

--- clusterTests.py
+++ clusterTests.py
@@ -15,7 +15,7 @@
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 
-from cluster import *
+from cluster import (HierarchicalClustering, KMeansClustering, ClusteringError)
 from difflib import SequenceMatcher
 import unittest
 
