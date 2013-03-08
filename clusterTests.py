63329976aaaf36b9be3498c806546a7fe73c7ca0
Michel Albert <michel@albert.lu>
2013-03-08 19:22:27 +0100

    PEP8 fixes for test script.

--- clusterTests.py
+++ clusterTests.py
@@ -19,6 +19,7 @@ from cluster import *
 from difflib import SequenceMatcher
 import unittest
 
+
 def compare_list(x, y):
     """
     Compare lists by content. Ordering does not matter.
@@ -41,17 +42,25 @@ def compare_list(x, y):
 
 
 class HClusterSmallListTestCase(unittest.TestCase):
-    " Test for Bug #1516204 "
+    """
+    Test for Bug #1516204
+    """
 
     def testClusterLen1(self):
-        "Testing if hierarchical clustering a set of length 1 returns a set of length 1"
-        cl = HierarchicalClustering([876], lambda x,y: abs(x-y))
+        """
+        Testing if hierarchical clustering a set of length 1 returns a set of
+        length 1
+        """
+        cl = HierarchicalClustering([876], lambda x, y: abs(x - y))
         self.assertEqual([876], cl.getlevel(40))
 
     def testClusterLen0(self):
-        "Testing if hierarchical clustering an empty list returns an empty list"
-        cl = HierarchicalClustering([], lambda x,y: abs(x-y))
+        """
+        Testing if hierarchical clustering an empty list returns an empty list
+        """
+        cl = HierarchicalClustering([], lambda x, y: abs(x - y))
         self.assertEqual([], cl.getlevel(40))
+
 
 class HClusterIntegerTestCase(unittest.TestCase):
 
@@ -61,9 +70,9 @@ class HClusterIntegerTestCase(unittest.TestCase):
 
     def testCluster(self):
         "Basic Hierarchical Clustering test with integers"
-        cl = HierarchicalClustering(self.__data, lambda x,y: abs(x-y))
+        cl = HierarchicalClustering(self.__data, lambda x, y: abs(x - y))
         cl.cluster()
-        self.assertEqual( [
+        self.assertEqual([
                 [24],
                 [84, 124, 131, 134],
                 [336, 365, 365, 365, 398, 391],
@@ -71,18 +80,20 @@ class HClusterIntegerTestCase(unittest.TestCase):
                 [791],
                 [835],
                 [676],
-                [518, 564, 542]
-                ],
+                [518, 564, 542]],
                 cl.getlevel(40))
+
 
 class HClusterStringTestCase(unittest.TestCase):
 
     def sim(self, x, y):
         sm = SequenceMatcher(lambda x: x in ". -", x, y)
-        return 1-sm.ratio()
+        return 1 - sm.ratio()
 
     def setUp(self):
-        self.__data = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut elit. Phasellus consequat ultricies mi. Sed congue leo at neque. Nullam.". split()
+        self.__data = ("Lorem ipsum dolor sit amet, consectetuer adipiscing "
+                "elit. Ut elit. Phasellus consequat ultricies mi. Sed congue "
+                "leo at neque. Nullam."). split()
 
     def testDataTypes(self):
         "Test for bug #?"
@@ -108,8 +119,8 @@ class HClusterStringTestCase(unittest.TestCase):
                     ['ipsum'],
                     ['adipiscing']
                     ],
-                cl.getlevel(0.5)
-                )
+                cl.getlevel(0.5))
+
 
 class KClusterSmallListTestCase(unittest.TestCase):
 
@@ -125,66 +136,81 @@ class KClusterSmallListTestCase(unittest.TestCase):
         self.assertEqual([], cl.getclusters(2))
         self.assertEqual([], cl.getclusters(7))
 
+
 class KCluster2DTestCase(unittest.TestCase):
 
     def testClusterCount(self):
         "Test that asking for less than 2 clusters raises an error"
-        cl = KMeansClustering([876, 123, 344, 676], distance=lambda x,y: abs(x-y))
+        cl = KMeansClustering([876, 123, 344, 676],
+                distance=lambda x, y: abs(x - y))
         self.assertRaises(ClusteringError, cl.getclusters, 0)
         self.assertRaises(ClusteringError, cl.getclusters, 1)
 
     def testNonsenseCluster(self):
-        "Test that asking for more clusters than data-items available raises an error"
-        cl = KMeansClustering([876, 123], distance=lambda x,y: abs(x-y))
+        """
+        Test that asking for more clusters than data-items available raises an
+        error
+        """
+        cl = KMeansClustering([876, 123], distance=lambda x, y: abs(x - y))
         self.assertRaises(ClusteringError, cl.getclusters, 5)
 
     def testUniformLength(self):
-        "Test if there is an item in the cluster that has a different cardinality"
-        data = [ (1,5), (2,5), (2,6), (3,4), (3,5), (3,6,7), (7,3), (8,1), (8,2), (8), (9,2), (9,3) ]
+        """
+        Test if there is an item in the cluster that has a different
+        cardinality
+        """
+        data = [(1, 5), (2, 5), (2, 6), (3, 4), (3, 5), (3, 6, 7), (7, 3), (8,
+            1), (8, 2), (8), (9, 2), (9, 3)]
         self.assertRaises(ValueError, KMeansClustering, data)
 
     def testPointDoubling(self):
         "test for bug #1604868"
-        data = [ (18,13), (15, 12), (17,12), (18,12), (19,12), (16,11), (18, 11),
-                 (19,10), (0,0), (1, 4), (1,2), (2,3), (4,1), (4,3), (5,2), (6,1)]
+        data = [(18, 13), (15, 12), (17, 12), (18, 12), (19, 12), (16, 11),
+                (18, 11), (19, 10), (0, 0), (1, 4), (1, 2), (2, 3), (4, 1),
+                (4, 3), (5, 2), (6, 1)]
         cl = KMeansClustering(data)
         clusters = cl.getclusters(2)
-        expected = [[(18,13), (15, 12), (17,12), (18,12), (19,12), (16,11), (18, 11), (19,10)],
-                 [(0,0), (1, 4), (1,2), (2,3), (4,1), (5,2), (6,1), (4,3)]]
-        self.assertTrue( compare_list(
+        expected = [[(18, 13), (15, 12), (17, 12), (18, 12), (19, 12), (16,
+            11), (18, 11), (19, 10)], [(0, 0), (1, 4), (1, 2), (2, 3), (4, 1),
+                (5, 2), (6, 1), (4, 3)]]
+        self.assertTrue(compare_list(
                 clusters,
-                expected ),
+                expected),
                 "Elements differ!\n%s\n%s" % (clusters, expected))
 
     def testClustering(self):
         "Basic clustering test"
-        data = [ (8,2), (7,3), (2,6), (3,5), (3,6), (1,5), (8,1), (3,4), (8,3), (9,2), (2,5), (9,3) ]
+        data = [(8, 2), (7, 3), (2, 6), (3, 5), (3, 6), (1, 5), (8, 1), (3,
+            4), (8, 3), (9, 2), (2, 5), (9, 3)]
         cl = KMeansClustering(data)
-        clusters = cl.getclusters(2)
         self.assertEqual(
                 cl.getclusters(2),
                 [[(8, 2), (8, 1), (8, 3), (7, 3), (9, 2), (9, 3)],
                  [(3, 5), (1, 5), (3, 4), (2, 6), (2, 5), (3, 6)]])
 
+
 class KClusterSFBugs(unittest.TestCase):
 
     def testLostFunctionReference(self):
         "test for bug #1727558"
-        cl = KMeansClustering([(1,1), (20,40), (20,41)], lambda x,y:x+y)
+        cl = KMeansClustering([(1, 1), (20, 40), (20, 41)],
+                lambda x, y: x + y)
         clusters = cl.getclusters(3)
-        expected = [(1,1), (20,40), (20,41)]
-        self.assertTrue( compare_list(
+        expected = [(1, 1), (20, 40), (20, 41)]
+        self.assertTrue(compare_list(
                 clusters,
-                expected ),
+                expected),
                 "Elements differ!\n%s\n%s" % (clusters, expected))
 
-unittest.TextTestRunner(verbosity=2).run(
-        unittest.TestSuite((
-                unittest.makeSuite(HClusterSmallListTestCase),
-                unittest.makeSuite(HClusterIntegerTestCase),
-                unittest.makeSuite(HClusterStringTestCase),
-                unittest.makeSuite(KClusterSmallListTestCase),
-                unittest.makeSuite(KCluster2DTestCase),
-                unittest.makeSuite(KClusterSFBugs),
-            ))
-        )
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(
+            unittest.TestSuite((
+                    unittest.makeSuite(HClusterSmallListTestCase),
+                    unittest.makeSuite(HClusterIntegerTestCase),
+                    unittest.makeSuite(HClusterStringTestCase),
+                    unittest.makeSuite(KClusterSmallListTestCase),
+                    unittest.makeSuite(KCluster2DTestCase),
+                    unittest.makeSuite(KClusterSFBugs),
+                ))
+            )
