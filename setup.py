5657f747a206b3c858df85e633a19c9d5eb72af4
Michel Albert <michel@albert.lu>
2013-03-11 08:00:30 +0100

    setup.py clenaup

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
-readmeContents = open("README").read()
-parastart = readmeContents.find('=\n')+3 # index where the first paragraph starts
+readme_contents = open("README").read()
 
-setup(name='cluster',
-      version='1.1.1b3',
-      author='Michel Albert',
-      author_email='exhuma@users.sourceforge.net',
-      url='http://python-cluster.sourceforge.net/',
-      py_modules=['cluster'],
-      license='LGPL',
-      description=readmeContents[parastart: readmeContents.find('.', parastart)], # first sentence of first paragraph
-      long_description = readmeContents
-      )
+# index where the first paragraph starts
+parastart = readme_contents.find('=\n') + 3
+
+# first sentence of first paragraph
+sentence_end = readme_contents.find('.', parastart)
+
+setup(
+    name='cluster',
+    version='1.1.1b3',
+    author='Michel Albert',
+    author_email='exhuma@users.sourceforge.net',
+    url='http://python-cluster.sourceforge.net/',
+    py_modules=['cluster'],
+    license='LGPL',
+    description=readme_contents[parastart:sentence_end],
+    long_description=readme_contents)
